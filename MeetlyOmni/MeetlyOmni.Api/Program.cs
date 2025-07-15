013438174d1b307fcf2e9e17d8069f8fa5fc71ac
depeng zhang <zhangdepeng3@gmail.com>
2025-07-15 13:08:19 +1000

    Update startup and project files for new database context and services

--- MeetlyOmni/MeetlyOmni.Api/Program.cs
+++ MeetlyOmni/MeetlyOmni.Api/Program.cs
@@ -1,4 +1,11 @@
+using MeetlyOmni.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Set up the database context with PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MeetlyOmniDb")));
 
 // Add services to the container.
 
