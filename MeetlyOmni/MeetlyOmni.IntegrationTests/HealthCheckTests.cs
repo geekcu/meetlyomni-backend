26073de4d7685895c70b1eb0dabbfab3513a80e0
yulin-ye <yulin.ye.australia@gmail.com>
2025-07-25 19:45:01 +1000

    Add the first integration test to test backend is launched correctly or not.

---
+++ MeetlyOmni/MeetlyOmni.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+
+namespace MeetlyOmni.IntegrationTests;
+public class BackendStatusTests
+{
+    [Fact]
+    public async Task Backend_Is_Available()
+    {
+        using var httpClient = new HttpClient
+        {
+            Timeout = TimeSpan.FromSeconds(30)
+        };
+
+        var baseUrl = LaunchSettingsReader.GetBaseUrl();
+        var response = await httpClient.GetAsync($"{baseUrl}/swagger/index.html");
+
+        Assert.True(response.IsSuccessStatusCode, $"Backend is not available. Status: {response.StatusCode}, Reason: {response.ReasonPhrase}");
+
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Contains("swagger", content.ToLowerInvariant());
+    }
+}
