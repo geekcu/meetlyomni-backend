9a4d3a40942e3870afda89be8873c93ea13d081f
depeng zhang <zhangdepeng3@gmail.com>
2025-08-04 16:29:21 +1000

    hotfix:remove the duplicated folder

--- src/MeetlyOmni.Unit.tests/UnitTest1.cs
+++ src/MeetlyOmni.Unit.tests/UnitTest1.cs
@@ -1,11 +1,12 @@
-namespace MeetlyOmni.Tests
+namespace MeetlyOmni.Unit.tests
 {
     public class UnitTest1
     {
         [Fact]
+        [Trait("Category", "Unit")]
         public void Test1()
         {
-
+            // your test code
         }
     }
 }
